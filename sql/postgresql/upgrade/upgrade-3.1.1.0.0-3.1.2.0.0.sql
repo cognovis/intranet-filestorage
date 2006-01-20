677a07b9e12d9c6d6171b898d82991a5c632ce8e
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-01-20 17:56:58 +0000

    - added privileges for fs root

--- sql/postgresql/upgrade/upgrade-3.1.1.0.0-3.1.2.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.1.1.0.0-3.1.2.0.0.sql
@@ -1,43 +1,52 @@
-
-
 -- -----------------------------------------------------
--- default_vat instead of vat
+-- Add permissions to handle the default permissions on
+-- empty filestorages
 
-alter table im_companies add default_vat numeric(12,1);
-alter table im_companies alter column default_vat set default 0;
-update im_companies set default_vat = vat;
-alter table im_companies drop column vat;
+select acs_privilege__create_privilege('fs_root_view','Default view permission for FS root?','');
+select acs_privilege__add_child('admin', 'fs_root_view');
 
+select acs_privilege__create_privilege('fs_root_read','Default read permission for FS root?','');
+select acs_privilege__add_child('admin', 'fs_root_read');
 
--- -----------------------------------------------------
--- default_invoice_template_id instead of invoice_template_id
+select acs_privilege__create_privilege('fs_root_write','Default write permission for FS root?','');
+select acs_privilege__add_child('admin', 'fs_root_write');
 
-alter table im_companies add
-        default_invoice_template_id integer
-        constraint im_companies_def_invoice_template_fk
-        references im_categories;
-
-update im_companies set default_invoice_template_id = invoice_template_id;
-alter table im_companies drop column invoice_template_id;
+select acs_privilege__create_privilege('fs_root_admin','Default admin permission for FS root?','');
+select acs_privilege__add_child('admin', 'fs_root_admin');
 
 
--- -----------------------------------------------------
--- Default payment method
-alter table im_companies add
-        default_payment_method_id       integer
-        constraint im_companies_def_invoice_payment_fk
-        references im_categories;
-
-update im_companies set default_payment_method_id = payment_method_id;
-alter table im_companies drop column payment_method_id;
+-- View Permissions
+--
+select im_priv_create('fs_root_view',        'Employees');
+select im_priv_create('fs_root_view',        'Accounting');
+select im_priv_create('fs_root_view',        'P/O Admins');
+select im_priv_create('fs_root_view',        'Project Managers');
+select im_priv_create('fs_root_view',        'Senior Managers');
 
 
--- -----------------------------------------------------
--- Default payment days
+-- Read Permissions
+--
+select im_priv_create('fs_root_read',        'Employees');
+select im_priv_create('fs_root_read',        'Accounting');
+select im_priv_create('fs_root_read',        'P/O Admins');
+select im_priv_create('fs_root_read',        'Project Managers');
+select im_priv_create('fs_root_read',        'Senior Managers');
 
-alter table im_companies add
-        default_payment_days            integer;
 
-update im_companies set default_payment_days = payment_days;
-alter table im_companies drop column payment_days;
+-- Write Permissions
+--
+select im_priv_create('fs_root_write',        'Employees');
+select im_priv_create('fs_root_write',        'Accounting');
+select im_priv_create('fs_root_write',        'P/O Admins');
+select im_priv_create('fs_root_write',        'Project Managers');
+select im_priv_create('fs_root_write',        'Senior Managers');
+
+
+-- Admin Permissions
+--
+select im_priv_create('fs_root_admin',        'Employees');
+select im_priv_create('fs_root_admin',        'Accounting');
+select im_priv_create('fs_root_admin',        'P/O Admins');
+select im_priv_create('fs_root_admin',        'Project Managers');
+select im_priv_create('fs_root_admin',        'Senior Managers');
 
